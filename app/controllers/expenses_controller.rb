4c948eb63a670ec633b7a5711421ee2d13da997d
Mauricio Miranda <maurimiranda@gmail.com>
2014-01-24 19:14:21 -0300

    added CUD actions to expenses

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,13 +1,47 @@
 class ExpensesController < ApplicationController
+  before_action :set_expense, only: [:show, :update, :destroy]
+  
   def index
     if params[:member_id]
       render json: Member.find(params[:member_id]).expenses
     else
-      render json: Expense.all.to_json(include: :payer)
+      render json: Expense.all
     end
   end
 
   def show
-    render json: Expense.find(params[:id]).to_json(include: :payer)
+    render json: Expense.find(params[:id])
   end
+
+  def create
+    expense = Expense.new(expense_params)
+
+    if expense.save
+      render json: expense, status: :created
+    else
+      render json: expense.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @expense.update(expense_params)
+      render json: @expense, status: :updated
+    else
+      render json: @expense.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @expense.destroy
+    head :no_content
+  end
+
+  private
+    def set_expense
+      @expense = Expense.find(params[:id])
+    end
+
+    def expense_params
+      params.require(:expense).permit(:title, :category, :provider, :comments, :amount, :date, :payer_id)
+    end
 end
--- app/models/expense.rb
+++ app/models/expense.rb
@@ -2,4 +2,10 @@ class Expense < ActiveRecord::Base
   belongs_to :payer, :class_name => 'Member', :foreign_key => 'payer_id'
   has_and_belongs_to_many :members
 
+  def serializable_hash(options={})
+    options = { 
+      include: :payer
+    }.update(options)
+    super(options)
+  end
 end
