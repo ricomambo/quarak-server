dced4ec2c6e475c586fd58f0f51c73065832bcb5
Mauricio Miranda <maurimiranda@gmail.com>
2014-04-28 21:34:52 -0300

    Created seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+expenses = Expense.create([
+  {date: "2014-03-25", category: "Supermarket", provider: "Walmart", amount: 745.24, payer: 1, comments: "Sample expense"},
+  {date: "2014-03-26", category: "Rent", provider: "Mr. Magoo", amount: 1400, payer: 2, comments: "Another expense"},
+  {date: "2014-04-27", category: "Supermarket", provider: "Carrefour", amount: 425.41, payer: 1, comments: "The last expense"}
+])
