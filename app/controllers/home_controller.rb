6cf633f75eb47244cfd01af9867b183998956163
Mauricio Miranda <maurimiranda@gmail.com>
2015-04-17 22:39:58 -0300

    Remove home

--- app/controllers/home_controller.rb
+++
@@ -1,8 +0,0 @@
-class HomeController < ApplicationController
-  skip_before_filter :authenticate_token!
-
-  def index
-    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML.new)
-    @readme = markdown.render(IO.read(Rails.root.join 'README.md')).html_safe
-  end
-end
