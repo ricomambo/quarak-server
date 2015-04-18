efc33435a70377d8ee09943ac41c5178c60a3d86
Mauricio Miranda <maurimiranda@gmail.com>
2015-04-17 22:40:15 -0300

    Add profile edition

--- config/routes.rb
+++ config/routes.rb
@@ -24,8 +24,6 @@ Rails.application.routes.draw do
     end
   end
 
-  root 'home#index'
-
   # Any other routes are handled here (as ActionDispatch prevents RoutingError from hitting ApplicationController::rescue_action).
   match "*path", :to => 'application#routing_error', :via => :all
 end
